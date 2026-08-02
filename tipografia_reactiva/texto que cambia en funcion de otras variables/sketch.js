eec7b8a30b4b80ff21c141e56f09904ba9936a33
eloysegura <111207907+eloysr@users.noreply.github.com>
2026-08-02 21:30:04 +0200

    Agregar archivos HTML y JavaScript para la tipografía reactiva basada en el micrófono

---
+++ tipografia_reactiva/texto que cambia en funcion de otras variables/sketch.js
@@ -0,0 +1,77 @@
+let interletrado = 15;
+let mic;
+let nivel = 0;
+let micActivo = false;
+let boton;
+let texto = "Cargando frase...";
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textFont("Helvetica");
+  textStyle(BOLD);
+  textSize(48);
+  textAlign(CENTER, CENTER);
+
+  mic = new p5.AudioIn();
+
+  boton = createButton("Activar micrófono");
+  boton.position(20, 20);
+  boton.mousePressed(activarMicrofono);
+
+  cargarFrase();
+  setInterval(cargarFrase, 30 * 1000); // nueva frase cada 30 segundos
+}
+
+function cargarFrase() {
+  loadJSON("https://api.quotable.io/random", (datos) => {
+    texto = datos.content.toUpperCase();
+  });
+}
+
+function activarMicrofono() {
+  userStartAudio();
+  mic.start();
+  micActivo = true;
+  boton.hide();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(20);
+  fill(255);
+
+  if (micActivo) {
+    nivel = mic.getLevel();
+  }
+
+  let interletradoActual = interletrado + nivel * 150;
+
+  let anchoTotal = 0;
+  for (let i = 0; i < texto.length; i++) {
+    anchoTotal += textWidth(texto.charAt(i)) + interletradoActual;
+  }
+  anchoTotal -= interletradoActual;
+
+  let x = width / 2 - anchoTotal / 2;
+  let y = height / 2;
+
+  for (let i = 0; i < texto.length; i++) {
+    let letra = texto.charAt(i);
+    let letraWidth = textWidth(letra);
+    let letraX = x + letraWidth / 2;
+
+    let desplazamiento = sin(frameCount * 0.2 + i) * nivel * 400;
+    let escala = 1 + nivel * 5;
+
+    push();
+    translate(letraX, y + desplazamiento);
+    scale(escala);
+    text(letra, 0, 0);
+    pop();
+
+    x += letraWidth + interletradoActual;
+  }
+}
